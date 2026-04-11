f9449b4f0fcdfa6218fde4d3a686b71fdb19f471
IGutkowski <162903836+IGutkowski@users.noreply.github.com>
2026-04-11 16:01:46 +0200

    init: vitacare

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Doctor from './components/Doctor'
+import Portfolio from './components/Portfolio'
+import Pricing from './components/Pricing'
+import Contact from './components/Contact'
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Doctor />
+      <Portfolio />
+      <Pricing />
+      <Contact />
+    </main>
+  )
+}
+
+export default App
